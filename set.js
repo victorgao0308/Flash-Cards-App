a58b753938bb128fb8e442791084130a4fedb71b
victor <victorgao0308@gmail.com>
2023-06-26 17:21:12 -0400

    sets are now stored in local storage

--- set.js
+++ set.js
@@ -5,6 +5,8 @@ const setsContainer = document.querySelector(".setsContainer");
 const setName = document.querySelector(".addSetInput");
 const cardsDiv = document.querySelector(".cardsDiv");
 
+const clearSetsBtn = document.querySelector(".clearSetsBtn");
+
 addSetBtn.addEventListener("click", () => {
   addSetMenu.classList.toggle("hideAddSetMenu");
 });
@@ -12,9 +14,8 @@ addSetBtn.addEventListener("click", () => {
 let setId = 1;
 let sets = [];
 
-// create a new set
+// handle creation of a new set
 createSetBtn.addEventListener("click", () => {
-
   let name = setName.value;
   let set = new Set(setId, name);
 
@@ -32,16 +33,16 @@ createSetBtn.addEventListener("click", () => {
   setName.value = "";
   setId++;
   addSetMenu.classList.add("hideAddSetMenu");
+  addSetToLocalStorage(set);
 });
 
-export class Set {
+class Set {
   constructor(id, name) {
     this.cards = [];
     this.id = id;
     this.name = name;
   }
 }
-
 
 function displaySet(set) {
   const addSetBtn = document.querySelector(".addSetBtn");
@@ -62,11 +63,43 @@ function displaySet(set) {
   <p>${set.id}</p>`;
 }
 
-
-
 // add set to local storage
 function addSetToLocalStorage(set) {
-  
+  let sets = getLocalStorage("sets");
+  sets.push(set);
+  localStorage.setItem("sets", JSON.stringify(sets));
+}
+
+// retrieve from local storage
+function getLocalStorage(item) {
+  return localStorage.getItem(item)
+    ? JSON.parse(localStorage.getItem(item))
+    : [];
+}
+
+window.addEventListener("load", loadSets);
+
+// load sets
+function loadSets() {
+  let sets = getLocalStorage("sets");
+  sets.forEach((set) => {
+    let newSet = document.createElement("div");
+    newSet.classList.add("set");
+    newSet.innerHTML = `<h2>${set.name}</h2>`;
+    setsContainer.appendChild(newSet);
+
+    newSet.addEventListener("click", () => {
+      let curSet = set;
+      console.log(set.cards);
+      displaySet(curSet);
+    });
+    setId++;
+  });
 }
 
 
+// clear sets
+clearSetsBtn.addEventListener('click', () => {
+  localStorage.clear();
+  setsContainer.innerHTML = "";
+})
