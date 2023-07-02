a5e1249712dc1c50c80aa659139ddf188f42bf08
victor <victorgao0308@gmail.com>
2023-07-01 22:05:40 -0400

    added framework for some study cards

--- studySet.js
+++ studySet.js
@@ -32,26 +32,85 @@ function loadCards() {
     currentCards.forEach(card => {
         cards.push(card);
     });
-    console.log(cards);
+    createStudyArray(cards);    
 }
 
 
+// create the order of cards in the studyCards array
+function createStudyArray(cards) {
+    let quizCards = [];
+    let indices = new Map();
+    shuffleArray(cards);
 
+    cards.forEach((card, index) => {
+        indices.set(card.cardId, index);
+        studyCards.push(new studyCard(card));
+        Math.random() > 0.5 ? quizCards.push(new multipleChoiceCard(card)) : quizCards.push(new fillInTheBlankCard(card));
+        Math.random() > 0.5 ? quizCards.push(new multipleChoiceCard(card)) : quizCards.push(new fillInTheBlankCard(card));
+    });
+
+    shuffleArray(quizCards);
+
+    quizCards.forEach(quizCard => {
+        let index = indices.get(quizCard.card.cardId);
+        let insertIndex = Math.floor(Math.random() * (studyCards.length - index)) + index + 1;
+        
+        studyCards.splice(insertIndex, 0, quizCard);
+        for (let [key, value] of indices) {
+            if (value >= insertIndex) {
+                indices.set(key, value + 1);
+            }
+        }
+    })
+
+    console.log(studyCards);
+}
+
+// randomize the elements of an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+class card {
+    constructor(card) {
+        this.card = card
+    }
+}
 // classes
-class studyCard {
+class studyCard extends card{
     constructor(card) {
-        this.card = card;
+        super(card);
     }
 }
 
-class multipleChoiceCard {
+class multipleChoiceCard extends card{
     constructor(card) {
-        this.card = card;
+        super(card);
     }
 }
 
-class fillInTheBlankCard {
+class fillInTheBlankCard extends card{
     constructor(card) {
-        this.card = card;
+        super(card);
     }
 }
+
+
+
+// for study cards
+
+// const studyCardElement = document.querySelector(".studyCard");
+// const cardFront = document.querySelector(".cardFront");
+// const cardBack = document.querySelector(".cardBack");
+// const cardType = document.querySelector(".cardType")
+
+// cardType.innerHTML = "Learn";
+
+// studyCardElement.addEventListener('click', () => {
+//     cardFront.classList.toggle('flipped');
+//     cardBack.classList.toggle('flipped');
+// })
